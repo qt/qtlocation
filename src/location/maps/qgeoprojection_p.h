qt/qtlocation

commit a316b25f4b4e0743e361031f2e6f1ba8072e704c

Fix qmake warnings
Change-Id: I17265b37a557472fc55fec7e25cd2bf2b3a6dc9d
Reviewed-by: Aaron McCarthy <mccarthy.aaron@gmail.com>

src/location/maps/qgeoprojection_p.h
@@ -57,7 +57,7 @@
 #endif
 
 #include "qglobal.h"
-#include "QtLocation/qlocationglobal.h"
+#include <QtLocation/QLocationGlobal>
 
 QT_BEGIN_NAMESPACE
 

src/location/maps/qgeocodingmanager.h
@@ -42,8 +42,8 @@
 #ifndef QGEOCODINGMANAGER_H
 #define QGEOCODINGMANAGER_H
 
-#include "qgeocodereply.h"
-#include "qgeorectangle.h"
+#include <QtLocation/QGeocodeReply>
+#include <QtLocation/QGeoRectangle>
 
 #include <QObject>
 #include <QList>

src/location/maps/qgeoroute.h
@@ -42,8 +42,8 @@
 #ifndef QGEOROUTE_H
 #define QGEOROUTE_H
 
-#include "qgeocoordinate.h"
-#include "qgeorouterequest.h"
+#include <QtLocation/QGeoCoordinate>
+#include <QtLocation/QGeoRouteRequest>
 
 #include <QExplicitlySharedDataPointer>
 #include <QList>

src/location/maps/qgeoroutereply.h
@@ -42,7 +42,7 @@
 #ifndef QGEOROUTEREPLY_H
 #define QGEOROUTEREPLY_H
 
-#include "qgeoroute.h"
+#include <QtLocation/QGeoRoute>
 
 #include <QList>
 #include <QObject>

src/location/maps/qgeoroutingmanager.h
@@ -42,8 +42,8 @@
 #ifndef QGEOROUTINGMANAGER_H
 #define QGEOROUTINGMANAGER_H
 
-#include "qgeorouterequest.h"
-#include "qgeoroutereply.h"
+#include <QtLocation/QGeoRouteRequest>
+#include <QtLocation/QGeoRouteReply>
 
 #include <QObject>
 #include <QMap>

src/location/maps/qgeoroutingmanagerengine.h
@@ -42,8 +42,8 @@
 #ifndef QGEOROUTINGMANAGERENGINE_H
 #define QGEOROUTINGMANAGERENGINE_H
 
-#include "qgeorouterequest.h"
-#include "qgeoroutereply.h"
+#include <QtLocation/QGeoRouteRequest>
+#include <QtLocation/QGeoRouteReply>
 
 #include <QObject>
 #include <QMap>

src/location/maps/qgeoserviceproviderfactory.h
@@ -42,7 +42,7 @@
 #ifndef QGEOSERVICEPROVIDERFACTORY_H
 #define QGEOSERVICEPROVIDERFACTORY_H
 
-#include "qgeoserviceprovider.h"
+#include <QtLocation/QGeoServiceProvider>
 
 #include <QtPlugin>
 #include <QMap>

src/location/places/qplace.h
@@ -43,18 +43,16 @@
 #define QPLACE_H
 
 #include <QtCore/QSharedDataPointer>
-#include <QtLocation/qtlocation.h>
-
-#include "qgeoaddress.h"
-#include "qgeorectangle.h"
-#include "qgeocoordinate.h"
-#include "qgeolocation.h"
-#include "qplacecategory.h"
-#include "qplacecontent.h"
-#include "qplaceratings.h"
-#include "qplacereview.h"
-#include "qplaceattribute.h"
-#include "qplacecontactdetail.h"
+#include <QtLocation/QGeoAddress>
+#include <QtLocation/QGeoRectangle>
+#include <QtLocation/QGeoCoordinate>
+#include <QtLocation/QGeoLocation>
+#include <QtLocation/QPlaceCategory>
+#include <QtLocation/QPlaceContent>
+#include <QtLocation/QPlaceRatings>
+#include <QtLocation/QPlaceReview>
+#include <QtLocation/QPlaceAttribute>
+#include <QtLocation/QPlaceContactDetail>
 
 QT_BEGIN_HEADER
 

commit a6c0789b1240a13b534f5ba5b4e3d57e3e504e01

Fix compile error because of missing header
Fixing an issue introduced with commit
a316b25f4b4e0743e361031f2e6f1ba8072e704c.

Change-Id: I227cfc07b135aad4367ef9a395389a08d8602270
Reviewed-by: Aaron McCarthy <mccarthy.aaron@gmail.com>

src/location/maps/qgeoprojection_p.h
@@ -57,7 +57,7 @@
 #endif
 
 #include "qglobal.h"
-#include <QtLocation/QLocationGlobal>
+#include <QtLocation/qlocationglobal.h>
 
 QT_BEGIN_NAMESPACE
 