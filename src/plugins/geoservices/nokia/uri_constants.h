3ca6b6c2ea271a8ba0719afd7e51b4d04fef4e4a
Alex Blasche <alexander.blasche@theqtcompany.com>
2015-02-25 15:48:41 +0100

    Remove all Nokia specific license details.
    The terms and conditions were not accurate anymore. Instead we refer to
    the relevant URL. In general the Nokia plugin is no different than
    other plugins. The user must comply with each services T&Cs.
    
    Nokia and Ovi don't exist anymore. The service runs under the Here
    term these days.
    
    Change-Id: Ie73b5cc9577946a2e1ce433ed67e0bca0b894a57
    Reviewed-by: Aaron McCarthy <mccarthy.aaron@gmail.com>

--- src/plugins/geoservices/nokia/uri_constants.h
+++ src/plugins/geoservices/nokia/uri_constants.h
@@ -33,13 +33,6 @@
 **
 ** $QT_END_LICENSE$
 **
-** This file is part of the Ovi services plugin for the Maps and
-** Navigation API.  The use of these services, whether by use of the
-** plugin or by other means, is governed by the terms and conditions
-** described by the file OVI_SERVICES_TERMS_AND_CONDITIONS.txt in
-** this package, located in the directory containing the Ovi services
-** plugin source code.
-**
 ****************************************************************************/
 #ifndef URI_CONSTANTS_H
 #define URI_CONSTANTS_H
