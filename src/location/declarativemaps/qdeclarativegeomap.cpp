qt/qtlocation

commit e81ba34a98b259723e783e2d2df4321309992291

Allow overzooming when setting zoomLevel directly in a Map
This patch moves the lower/upper bound check on setZoom from
QDeclarativeGeoMap to the gesture area, allowing to set higher zoom
levels than the maximumZoomLevel when setting Map.zoomLevel directly,
for the map types that support overzoom.
This is now safe as the bound check is introduced in the tile fetcher,
so no invalid tiles will be requested, and is beneficial when
combining layers supporting different maximum zoom levels.

Change-Id: I08ee9c282ee2ebc1dafa3c68a238b93ffbc1ba02
Reviewed-by: Alex Blasche <alexander.blasche@qt.io>

src/location/declarativemaps/qdeclarativegeomap.cpp
@@ -865,7 +865,7 @@ void QDeclarativeGeoMap::setMinimumZoomLevel(qreal minimumZoomLevel, bool userSe
 
         m_gestureArea->setMinimumZoomLevel(minimumZoomLevel);
 
-        if (zoomLevel() < minimumZoomLevel)
+        if (zoomLevel() < minimumZoomLevel && (m_gestureArea->enabled() || !m_cameraCapabilities.overzoomEnabled()))
             setZoomLevel(minimumZoomLevel);
 
         if (oldMinimumZoomLevel != minimumZoomLevel)
@@ -908,7 +908,7 @@ void QDeclarativeGeoMap::setMaximumZoomLevel(qreal maximumZoomLevel, bool userSe
 
         m_gestureArea->setMaximumZoomLevel(maximumZoomLevel);
 
-        if (zoomLevel() > maximumZoomLevel)
+        if (zoomLevel() > maximumZoomLevel && (m_gestureArea->enabled() || !m_cameraCapabilities.overzoomEnabled()))
             setZoomLevel(maximumZoomLevel);
 
         if (oldMaximumZoomLevel != maximumZoomLevel)
@@ -947,7 +947,8 @@ void QDeclarativeGeoMap::setZoomLevel(qreal zoomLevel)
     bool centerHasChanged = false;
 
     if (m_initialized) {
-        m_cameraData.setZoomLevel(qBound(minimumZoomLevel(), zoomLevel, maximumZoomLevel()));
+        m_cameraData.setZoomLevel(qBound<qreal>(m_cameraCapabilities.overzoomEnabled() ? 0 : minimumZoomLevel(), zoomLevel,
+                                                m_cameraCapabilities.overzoomEnabled() ? 30 : maximumZoomLevel()));
         m_maximumViewportLatitude = m_map->maximumCenterLatitudeAtZoom(m_cameraData);
         QGeoCoordinate coord = m_cameraData.center();
         coord.setLatitude(qBound(-m_maximumViewportLatitude, coord.latitude(), m_maximumViewportLatitude));

src/location/declarativemaps/qquickgeomapgesturearea.cpp
@@ -949,7 +949,8 @@ void QQuickGeoMapGestureArea::handleWheelEvent(QWheelEvent *event)
     const QPointF &preZoomPoint = event->posF();
 
     const double zoomLevelDelta = event->angleDelta().y() * qreal(0.001);
-    m_declarativeMap->setZoomLevel(m_declarativeMap->zoomLevel() + zoomLevelDelta);
+    // Gesture area should always honor maxZL, but Map might not.
+    m_declarativeMap->setZoomLevel(qMin<qreal>(m_declarativeMap->zoomLevel() + zoomLevelDelta, maximumZoomLevel()));
     const QPointF &postZoomPoint = m_map->geoProjection().coordinateToItemPosition(wheelGeoPos, false).toPointF();
 
     if (preZoomPoint != postZoomPoint) // need to re-anchor the wheel geoPos to the event position
@@ -1590,7 +1591,7 @@ void QQuickGeoMapGestureArea::updatePinch()
         qreal perPinchMinimumZoomLevel = qMax(m_pinch.m_zoom.m_start - m_pinch.m_zoom.maximumChange, m_pinch.m_zoom.m_minimum);
         qreal perPinchMaximumZoomLevel = qMin(m_pinch.m_zoom.m_start + m_pinch.m_zoom.maximumChange, m_pinch.m_zoom.m_maximum);
         newZoomLevel = qMin(qMax(perPinchMinimumZoomLevel, newZoomLevel), perPinchMaximumZoomLevel);
-        m_declarativeMap->setZoomLevel(newZoomLevel);
+        m_declarativeMap->setZoomLevel(qMin<qreal>(newZoomLevel, maximumZoomLevel()));
         m_pinch.m_zoom.m_previous = newZoomLevel;
     }
 }

src/location/maps/qgeocameracapabilities.cpp
@@ -74,6 +74,7 @@ public:
     int tileSize_;
     double minimumFieldOfView_;
     double maximumFieldOfView_;
+    bool overzoomEnabled_;
 };
 
 QGeoCameraCapabilitiesPrivate::QGeoCameraCapabilitiesPrivate()
@@ -87,7 +88,8 @@ QGeoCameraCapabilitiesPrivate::QGeoCameraCapabilitiesPrivate()
       maxTilt_(0.0),
       tileSize_(256),
       minimumFieldOfView_(45.0),  // Defaulting to a fixed FOV of 45 degrees. Too large FOVs cause the loading of too many tiles
-      maximumFieldOfView_(45.0) {}
+      maximumFieldOfView_(45.0),
+      overzoomEnabled_(false) {}
 
 
 QGeoCameraCapabilitiesPrivate::QGeoCameraCapabilitiesPrivate(const QGeoCameraCapabilitiesPrivate &other)
@@ -102,7 +104,8 @@ QGeoCameraCapabilitiesPrivate::QGeoCameraCapabilitiesPrivate(const QGeoCameraCap
       maxTilt_(other.maxTilt_),
       tileSize_(other.tileSize_),
       minimumFieldOfView_(other.minimumFieldOfView_),
-      maximumFieldOfView_(other.maximumFieldOfView_) {}
+      maximumFieldOfView_(other.maximumFieldOfView_),
+      overzoomEnabled_(other.overzoomEnabled_){}
 
 
 QGeoCameraCapabilitiesPrivate::~QGeoCameraCapabilitiesPrivate() {}
@@ -123,6 +126,7 @@ QGeoCameraCapabilitiesPrivate &QGeoCameraCapabilitiesPrivate::operator = (const 
     tileSize_ = other.tileSize_;
     minimumFieldOfView_ = other.minimumFieldOfView_;
     maximumFieldOfView_ = other.maximumFieldOfView_;
+    overzoomEnabled_ = other.overzoomEnabled_;
 
     return *this;
 }
@@ -139,7 +143,8 @@ bool QGeoCameraCapabilitiesPrivate::operator == (const QGeoCameraCapabilitiesPri
             && (maxTilt_ == rhs.maxTilt_)
             && (tileSize_ == rhs.tileSize_)
             && (minimumFieldOfView_ == rhs.minimumFieldOfView_)
-            && (maximumFieldOfView_ == rhs.maximumFieldOfView_));
+            && (maximumFieldOfView_ == rhs.maximumFieldOfView_)
+            && (overzoomEnabled_ == rhs.overzoomEnabled_));
 }
 
 /*!
@@ -428,6 +433,27 @@ void QGeoCameraCapabilities::setMaximumFieldOfView(double maximumFieldOfView)
 double QGeoCameraCapabilities::maximumFieldOfView() const
 {
     return d->maximumFieldOfView_;
+}
+
+/*!
+    Sets whether overzooming is supported by the associated plugin.
+
+    \since 5.9
+*/
+void QGeoCameraCapabilities::setOverzoomEnabled(bool overzoomEnabled)
+{
+    d->overzoomEnabled_ = overzoomEnabled;
+    d->valid_ = true;
+}
+
+/*!
+    Returns whether overzooming is supported by the associated plugin.
+
+    \since 5.9
+*/
+bool QGeoCameraCapabilities::overzoomEnabled() const
+{
+    return d->overzoomEnabled_;
 }
 
 

src/location/maps/qgeocameracapabilities_p.h
@@ -100,6 +100,9 @@ public:
     void setMaximumFieldOfView(double maximumFieldOfView);
     double maximumFieldOfView() const;
 
+    void setOverzoomEnabled(bool overzoomEnabled);
+    bool overzoomEnabled() const;
+
     bool isValid() const;
 
 private:

src/plugins/geoservices/esri/geotiledmappingmanagerengine_esri.cpp
@@ -101,6 +101,7 @@ GeoTiledMappingManagerEngineEsri::GeoTiledMappingManagerEngineEsri(const QVarian
     cameraCaps.setMaximumTilt(80);
     cameraCaps.setMinimumFieldOfView(20.0);
     cameraCaps.setMaximumFieldOfView(120.0);
+    cameraCaps.setOverzoomEnabled(true);
     setCameraCapabilities(cameraCaps);
 
     setTileSize(QSize(256, 256));

src/plugins/geoservices/mapbox/qgeotiledmappingmanagerenginemapbox.cpp
@@ -56,6 +56,7 @@ QGeoTiledMappingManagerEngineMapbox::QGeoTiledMappingManagerEngineMapbox(const Q
     cameraCaps.setMaximumTilt(80);
     cameraCaps.setMinimumFieldOfView(20.0);
     cameraCaps.setMaximumFieldOfView(120.0);
+    cameraCaps.setOverzoomEnabled(true);
     setCameraCapabilities(cameraCaps);
 
     setTileSize(QSize(256, 256));

src/plugins/geoservices/nokia/qgeotiledmappingmanagerengine_nokia.cpp
@@ -85,6 +85,7 @@ QGeoTiledMappingManagerEngineNokia::QGeoTiledMappingManagerEngineNokia(
     capabilities.setMaximumTilt(80);
     capabilities.setMinimumFieldOfView(20.0);
     capabilities.setMaximumFieldOfView(120.0);
+    capabilities.setOverzoomEnabled(true);
     setCameraCapabilities(capabilities);
 
     setTileSize(QSize(256, 256));

src/plugins/geoservices/osm/qgeotiledmappingmanagerengineosm.cpp
@@ -63,6 +63,7 @@ QGeoTiledMappingManagerEngineOsm::QGeoTiledMappingManagerEngineOsm(const QVarian
     cameraCaps.setMaximumTilt(80);
     cameraCaps.setMinimumFieldOfView(20.0);
     cameraCaps.setMaximumFieldOfView(120.0);
+    cameraCaps.setOverzoomEnabled(true);
     setCameraCapabilities(cameraCaps);
 
     setTileSize(QSize(256, 256));

commit 18f7e6dc9f59159b70cb7e6a8745738f42d4f46f

Fix for MapGestureArea pinch and scroll wheel not honoring min zoom
This patch fixes a regression introduced with
e81ba34a98b259723e783e2d2df4321309992291, which made possible to
zoom below the minimum valid zoom level for a viewport size.

Change-Id: I4e71530b9bc5423ea24083146963c3bd0928f14a
Reviewed-by: Alex Blasche <alexander.blasche@qt.io>

src/location/declarativemaps/qdeclarativegeomap.cpp
@@ -942,6 +942,21 @@ qreal QDeclarativeGeoMap::maximumZoomLevel() const
 */
 void QDeclarativeGeoMap::setZoomLevel(qreal zoomLevel)
 {
+    return setZoomLevel(zoomLevel, m_cameraCapabilities.overzoomEnabled());
+}
+
+/*!
+    \internal
+
+    Sets the zoom level.
+    Larger values for the zoom level provide more detail. Zoom levels
+    are always non-negative. The default value is 8.0. Values outside the
+    [minimumZoomLevel, maximumZoomLevel] range, which represent the range for which
+    tiles are available, can be accepted or clamped by setting the overzoom argument
+    to true or false respectively.
+*/
+void QDeclarativeGeoMap::setZoomLevel(qreal zoomLevel, bool overzoom)
+{
     if (m_cameraData.zoomLevel() == zoomLevel || zoomLevel < 0)
         return;
 
@@ -949,8 +964,9 @@ void QDeclarativeGeoMap::setZoomLevel(qreal zoomLevel)
     bool centerHasChanged = false;
 
     if (m_initialized) {
-        m_cameraData.setZoomLevel(qBound<qreal>(m_cameraCapabilities.overzoomEnabled() ? 0 : minimumZoomLevel(), zoomLevel,
-                                                m_cameraCapabilities.overzoomEnabled() ? 30 : maximumZoomLevel()));
+        m_cameraData.setZoomLevel(qBound<qreal>(overzoom ? m_map->minimumZoom() : minimumZoomLevel(),
+                                                zoomLevel,
+                                                overzoom ? 30 : maximumZoomLevel()));
         m_maximumViewportLatitude = m_map->maximumCenterLatitudeAtZoom(m_cameraData);
         QGeoCoordinate coord = m_cameraData.center();
         coord.setLatitude(qBound(-m_maximumViewportLatitude, coord.latitude(), m_maximumViewportLatitude));

src/location/declarativemaps/qdeclarativegeomap_p.h
@@ -228,6 +228,8 @@ protected:
 
     void setError(QGeoServiceProvider::Error error, const QString &errorString);
     void initialize();
+    void setZoomLevel(qreal zoomLevel, bool overzoom);
+
 private Q_SLOTS:
     void mappingManagerInitialized();
     void pluginReady();

src/location/declarativemaps/qquickgeomapgesturearea.cpp
@@ -953,7 +953,8 @@ void QQuickGeoMapGestureArea::handleWheelEvent(QWheelEvent *event)
 
     const double zoomLevelDelta = event->angleDelta().y() * qreal(0.001);
     // Gesture area should always honor maxZL, but Map might not.
-    m_declarativeMap->setZoomLevel(qMin<qreal>(m_declarativeMap->zoomLevel() + zoomLevelDelta, maximumZoomLevel()));
+    m_declarativeMap->setZoomLevel(qMin<qreal>(m_declarativeMap->zoomLevel() + zoomLevelDelta, maximumZoomLevel()),
+                                   false);
     const QPointF &postZoomPoint = m_map->geoProjection().coordinateToItemPosition(wheelGeoPos, false).toPointF();
 
     if (preZoomPoint != postZoomPoint) // need to re-anchor the wheel geoPos to the event position
@@ -1595,7 +1596,7 @@ void QQuickGeoMapGestureArea::updatePinch()
         qreal perPinchMinimumZoomLevel = qMax(m_pinch.m_zoom.m_start - m_pinch.m_zoom.maximumChange, m_pinch.m_zoom.m_minimum);
         qreal perPinchMaximumZoomLevel = qMin(m_pinch.m_zoom.m_start + m_pinch.m_zoom.maximumChange, m_pinch.m_zoom.m_maximum);
         newZoomLevel = qMin(qMax(perPinchMinimumZoomLevel, newZoomLevel), perPinchMaximumZoomLevel);
-        m_declarativeMap->setZoomLevel(qMin<qreal>(newZoomLevel, maximumZoomLevel()));
+        m_declarativeMap->setZoomLevel(qMin<qreal>(newZoomLevel, maximumZoomLevel()), false);
         m_pinch.m_zoom.m_previous = newZoomLevel;
     }
 }