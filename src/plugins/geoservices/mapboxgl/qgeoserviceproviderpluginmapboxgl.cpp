8552cf9d96d96dc500743dfbc117f0634cab0590
Bruno de Oliveira Abinader <bruno@mapbox.com>
2017-11-20 19:28:39 +0200

    Remove OpenGL ES limitation for Mapbox GL on Windows
    Also fixes an issue preventing GL native from working on MinGW.
    
    mapbox-gl-native @ 8757164ac8f2b033b2b12d4baf075ed18cfeb2b4
    
    Task-number: QTBUG-64632
    Change-Id: Iab9a560adcd54194cd9829a326b325856f8c8cc2
    Reviewed-by: Paolo Angelelli <paolo.angelelli@qt.io>

--- src/plugins/geoservices/mapboxgl/qgeoserviceproviderpluginmapboxgl.cpp
+++ src/plugins/geoservices/mapboxgl/qgeoserviceproviderpluginmapboxgl.cpp
@@ -65,15 +65,6 @@ QGeoCodingManagerEngine *QGeoServiceProviderFactoryMapboxGL::createGeocodingMana
 QGeoMappingManagerEngine *QGeoServiceProviderFactoryMapboxGL::createMappingManagerEngine(
     const QVariantMap &parameters, QGeoServiceProvider::Error *error, QString *errorString) const
 {
-#ifdef Q_OS_WIN
-    if (QOpenGLContext::openGLModuleType() != QOpenGLContext::LibGLES) {
-        qWarning("The Mapbox GL plugin only supports OpenGL ES on Windows. \
-            Try setting the environment variable QT_OPENGL to 'angle'.");
-
-        return 0;
-    }
-#endif
-
     return new QGeoMappingManagerEngineMapboxGL(parameters, error, errorString);
 }
 
