qt/qtlocation

commit 69a42c4a5c37a5a74c4b285c64328bc88ed8e059

Allow plugins to use alternative map projections
QtLocation mapping has always been geared around the WebMercator
projection. Some mapping SDKs support additional projections, such
as General Perspective (often called globe view or globe rendering).

The goal of this patch is to allow a plugin to provide such a view,
disabling WebMercator specific features, and redirecting API calls
to plugin-specific implementations.

In particular, this patch disables the rendering of Map Items
(QDeclarativeGeoMapItemBase and sons) for projections different from
WebMercator, with the exception of MapQuickItems.
MapQuickItems, in turn, lose the ability to draw "on the map", when
the projection is different from WebMercator. However, they can still
be used to add geolocated popups, buttons and other UI elements.

fitViewportToMapItems is also disabled, for both it can't be computed
and there wouldn't be any item to fit (with the exception of
mapquickitems)

Change-Id: I9fa2fdd01a35a078fc4663efc9d269c4ecaa3f41
Reviewed-by: Alex Blasche <alexander.blasche@qt.io>

src/location/maps/qgeomap.cpp
@@ -91,7 +91,7 @@ void QGeoMap::setCameraData(const QGeoCameraData &cameraData)
     if (cameraData == d->m_cameraData)
         return;
     d->m_cameraData = cameraData;
-    d->m_geoProjection->setCameraData(cameraData);
+    d->m_geoProjection->setCameraData(cameraData, false);
     d->changeCameraData(cameraData);
     emit cameraDataChanged(d->m_cameraData);
 }
@@ -106,6 +106,40 @@ bool QGeoMap::handleEvent(QEvent *event)
 {
     Q_UNUSED(event)
     return false;
+}
+
+bool QGeoMap::setBearing(qreal bearing, const QGeoCoordinate &coordinate)
+{
+    Q_D(QGeoMap);
+    bool res = d->m_geoProjection->setBearing(bearing, coordinate);
+    if (!res)
+        return false;
+
+    setCameraData(geoProjection().cameraData());
+    return true;
+}
+
+bool QGeoMap::anchorCoordinateToPoint(const QGeoCoordinate &coordinate, const QPointF &anchorPoint)
+{
+    QGeoCoordinate newCenter = geoProjection().anchorCoordinateToPoint(coordinate, anchorPoint);
+    QGeoCameraData data = cameraData();
+    if (data.center() != newCenter) {
+        data.setCenter(newCenter);
+        setCameraData(data);
+        return true;
+    }
+    return false;
+}
+
+bool QGeoMap::fitViewportToGeoRectangle(const QGeoRectangle &rectangle)
+{
+    Q_UNUSED(rectangle)
+    return false;
+}
+
+QGeoShape QGeoMap::visibleRegion() const
+{
+    return geoProjection().visibleRegion();
 }
 
 QGeoCameraData QGeoMap::cameraData() const
@@ -146,13 +180,13 @@ double QGeoMap::maximumCenterLatitudeAtZoom(const QGeoCameraData &cameraData) co
 double QGeoMap::mapWidth() const
 {
     Q_D(const QGeoMap);
-    return d->m_geoProjection->mapWidth();
+    return d->mapWidth();
 }
 
 double QGeoMap::mapHeight() const
 {
     Q_D(const QGeoMap);
-    return d->m_geoProjection->mapHeight();
+    return d->mapHeight();
 }
 
 const QGeoProjection &QGeoMap::geoProjection() const
@@ -165,6 +199,11 @@ QGeoCameraCapabilities QGeoMap::cameraCapabilities() const
 {
     Q_D(const QGeoMap);
     return d->m_cameraCapabilities;
+}
+
+QGeoMap::Capabilities QGeoMap::capabilities() const
+{
+    return Capabilities(QGeoMap::SupportsNothing);
 }
 
 void QGeoMap::prefetchData()
@@ -335,6 +374,20 @@ bool QGeoMapPrivate::createMapObjectImplementation(QGeoMapObject *obj)
 QList<QGeoMapObject *> QGeoMapPrivate::mapObjects() const
 {
     return QList<QGeoMapObject *>();
+}
+
+double QGeoMapPrivate::mapWidth() const
+{
+    if (m_geoProjection->projectionType() == QGeoProjection::ProjectionWebMercator)
+        return static_cast<const QGeoProjectionWebMercator *>(m_geoProjection)->mapWidth();
+    return 0; // override this for maps supporting other projections
+}
+
+double QGeoMapPrivate::mapHeight() const
+{
+    if (m_geoProjection->projectionType() == QGeoProjection::ProjectionWebMercator)
+        return static_cast<const QGeoProjectionWebMercator *>(m_geoProjection)->mapHeight();
+    return 0; // override this for maps supporting other projections
 }
 
 QT_END_NAMESPACE

src/location/maps/qgeomap_p_p.h
@@ -96,6 +96,9 @@ protected:
     virtual void changeCameraData(const QGeoCameraData &oldCameraData) = 0; // called by QGeoMap::setCameraData()
     virtual void changeActiveMapType(const QGeoMapType mapType) = 0; // called by QGeoMap::setActiveMapType()
 
+    virtual double mapWidth() const;
+    virtual double mapHeight() const;
+
 protected:
     QSize m_viewportSize;
     QGeoProjection *m_geoProjection;

src/location/declarativemaps/qdeclarativecirclemapitem.cpp
@@ -144,6 +144,7 @@ QGeoMapCircleGeometry::QGeoMapCircleGeometry()
 */
 void QGeoMapCircleGeometry::updateScreenPointsInvert(const QList<QDoubleVector2D> &circlePath, const QGeoMap &map)
 {
+    const QGeoProjectionWebMercator &p = static_cast<const QGeoProjectionWebMercator&>(map.geoProjection());
     // Not checking for !screenDirty anymore, as everything is now recalculated.
     clear();
     if (map.viewportWidth() == 0 || map.viewportHeight() == 0 || circlePath.size() < 3) // a circle requires at least 3 points;
@@ -160,23 +161,23 @@ void QGeoMapCircleGeometry::updateScreenPointsInvert(const QList<QDoubleVector2D
      */
 
     // 1)
-    double topLati = QLocationUtils::mercatorMaxLatitude();
-    double bottomLati = -(QLocationUtils::mercatorMaxLatitude());
-    double leftLongi = QLocationUtils::mapLeftLongitude(map.cameraData().center().longitude());
-    double rightLongi = QLocationUtils::mapRightLongitude(map.cameraData().center().longitude());
+    const double topLati = QLocationUtils::mercatorMaxLatitude();
+    const double bottomLati = -(QLocationUtils::mercatorMaxLatitude());
+    const double leftLongi = QLocationUtils::mapLeftLongitude(map.cameraData().center().longitude());
+    const double rightLongi = QLocationUtils::mapRightLongitude(map.cameraData().center().longitude());
 
     srcOrigin_ = QGeoCoordinate(topLati,leftLongi);
-    QDoubleVector2D tl = map.geoProjection().geoToWrappedMapProjection(QGeoCoordinate(topLati,leftLongi));
-    QDoubleVector2D tr = map.geoProjection().geoToWrappedMapProjection(QGeoCoordinate(topLati,rightLongi));
-    QDoubleVector2D br = map.geoProjection().geoToWrappedMapProjection(QGeoCoordinate(bottomLati,rightLongi));
-    QDoubleVector2D bl = map.geoProjection().geoToWrappedMapProjection(QGeoCoordinate(bottomLati,leftLongi));
+    const QDoubleVector2D tl = p.geoToWrappedMapProjection(QGeoCoordinate(topLati,leftLongi));
+    const QDoubleVector2D tr = p.geoToWrappedMapProjection(QGeoCoordinate(topLati,rightLongi));
+    const QDoubleVector2D br = p.geoToWrappedMapProjection(QGeoCoordinate(bottomLati,rightLongi));
+    const QDoubleVector2D bl = p.geoToWrappedMapProjection(QGeoCoordinate(bottomLati,leftLongi));
 
     QList<QDoubleVector2D> fill;
     fill << tl << tr << br << bl;
 
     QList<QDoubleVector2D> hole;
     for (const QDoubleVector2D &c: circlePath)
-        hole << map.geoProjection().wrapMapProjection(c);
+        hole << p.wrapMapProjection(c);
 
     c2t::clip2tri clipper;
     clipper.addSubjectPath(QClipperUtils::qListToPath(fill), true);
@@ -184,9 +185,9 @@ void QGeoMapCircleGeometry::updateScreenPointsInvert(const QList<QDoubleVector2D
     Paths difference = clipper.execute(c2t::clip2tri::Difference, QtClipperLib::pftEvenOdd, QtClipperLib::pftEvenOdd);
 
     // 2)
-    QDoubleVector2D lb = map.geoProjection().geoToWrappedMapProjection(srcOrigin_);
+    QDoubleVector2D lb = p.geoToWrappedMapProjection(srcOrigin_);
     QList<QList<QDoubleVector2D> > clippedPaths;
-    const QList<QDoubleVector2D> &visibleRegion = map.geoProjection().visibleRegion();
+    const QList<QDoubleVector2D> &visibleRegion = p.visibleGeometry();
     if (visibleRegion.size()) {
         clipper.clearClipper();
         for (const Path &p: difference)
@@ -210,19 +211,19 @@ void QGeoMapCircleGeometry::updateScreenPointsInvert(const QList<QDoubleVector2D
         // Prevent the conversion to and from clipper from introducing negative offsets which
         // in turn will make the geometry wrap around.
         lb.setX(qMax(tl.x(), lb.x()));
-        srcOrigin_ = map.geoProjection().mapProjectionToGeo(map.geoProjection().unwrapMapProjection(lb));
+        srcOrigin_ = p.mapProjectionToGeo(p.unwrapMapProjection(lb));
     } else {
         clippedPaths = QClipperUtils::pathsToQList(difference);
     }
 
     //3)
-    QDoubleVector2D origin = map.geoProjection().wrappedMapProjectionToItemPosition(lb);
+    QDoubleVector2D origin = p.wrappedMapProjectionToItemPosition(lb);
 
     QPainterPath ppi;
     for (const QList<QDoubleVector2D> &path: clippedPaths) {
         QDoubleVector2D lastAddedPoint;
         for (int i = 0; i < path.size(); ++i) {
-            QDoubleVector2D point = map.geoProjection().wrappedMapProjectionToItemPosition(path.at(i));
+            QDoubleVector2D point = p.wrappedMapProjectionToItemPosition(path.at(i));
             //point = point - origin; // Do this using ppi.translate()
 
             if (i == 0) {
@@ -483,9 +484,11 @@ QSGNode *QDeclarativeCircleMapItem::updateMapItemPaintNode(QSGNode *oldNode, Upd
 */
 void QDeclarativeCircleMapItem::updatePolish()
 {
-    if (!map() || !circle_.isValid())
+    if (!map() || !circle_.isValid()
+            || map()->geoProjection().projectionType() != QGeoProjection::ProjectionWebMercator)
         return;
 
+    const QGeoProjectionWebMercator &p = static_cast<const QGeoProjectionWebMercator&>(map()->geoProjection());
     QScopedValueRollback<bool> rollback(updatingGeometry_);
     updatingGeometry_ = true;
 
@@ -533,7 +536,7 @@ void QDeclarativeCircleMapItem::updatePolish()
         QDoubleVector2D borderLeftBoundWrapped;
         QList<QList<QDoubleVector2D > > clippedPaths = borderGeometry_.clipPath(*map(), closedPath, borderLeftBoundWrapped);
         if (clippedPaths.size()) {
-            borderLeftBoundWrapped = map()->geoProjection().geoToWrappedMapProjection(geometryOrigin);
+            borderLeftBoundWrapped = p.geoToWrappedMapProjection(geometryOrigin);
             borderGeometry_.pathToScreen(*map(), clippedPaths, borderLeftBoundWrapped);
             borderGeometry_.updateScreenPoints(*map(), border_.width());
             geoms << &borderGeometry_;
@@ -565,13 +568,15 @@ void QDeclarativeCircleMapItem::afterViewportChanged(const QGeoMapViewportChange
 */
 void QDeclarativeCircleMapItem::updateCirclePath()
 {
-    if (!map())
+    if (!map() || map()->geoProjection().projectionType() != QGeoProjection::ProjectionWebMercator)
         return;
+
+    const QGeoProjectionWebMercator &p = static_cast<const QGeoProjectionWebMercator&>(map()->geoProjection());
     QList<QGeoCoordinate> path;
     calculatePeripheralPoints(path, circle_.center(), circle_.radius(), CircleSamples, leftBound_);
     circlePath_.clear();
     for (const QGeoCoordinate &c : path)
-        circlePath_ << map()->geoProjection().geoToMapProjection(c);
+        circlePath_ << p.geoToMapProjection(c);
 }
 
 /*!
@@ -644,6 +649,7 @@ void QDeclarativeCircleMapItem::updateCirclePathForRendering(QList<QDoubleVector
                                                              const QGeoCoordinate &center,
                                                              qreal distance)
 {
+    const QGeoProjectionWebMercator &p = static_cast<const QGeoProjectionWebMercator&>(map()->geoProjection());
     const qreal poleLat = 90;
     const qreal distanceToNorthPole = center.distanceTo(QGeoCoordinate(poleLat, 0));
     const qreal distanceToSouthPole = center.distanceTo(QGeoCoordinate(-poleLat, 0));
@@ -651,11 +657,11 @@ void QDeclarativeCircleMapItem::updateCirclePathForRendering(QList<QDoubleVector
     bool crossSouthPole = distanceToSouthPole < distance;
 
     QList<int> wrapPathIndex;
-    QDoubleVector2D prev = map()->geoProjection().wrapMapProjection(path.at(0));
+    QDoubleVector2D prev = p.wrapMapProjection(path.at(0));
 
     for (int i = 1; i <= path.count(); ++i) {
         int index = i % path.count();
-        QDoubleVector2D point = map()->geoProjection().wrapMapProjection(path.at(index));
+        QDoubleVector2D point = p.wrapMapProjection(path.at(index));
         double diff = qAbs(point.x() - prev.x());
         if (diff > 0.5) {
             continue;
@@ -665,7 +671,7 @@ void QDeclarativeCircleMapItem::updateCirclePathForRendering(QList<QDoubleVector
     // find the points in path where wrapping occurs
     for (int i = 1; i <= path.count(); ++i) {
         int index = i % path.count();
-        QDoubleVector2D point = map()->geoProjection().wrapMapProjection(path.at(index));
+        QDoubleVector2D point = p.wrapMapProjection(path.at(index));
         if ( (qAbs(point.x() - prev.x())) >= 0.5 ) {
             wrapPathIndex << index;
             if (wrapPathIndex.size() == 2 || !(crossNorthPole && crossSouthPole))

commit 1e6a5349def9c6e3e929fc57949fc97e267af37d

Fix regression: gesture area not honoring maximum center latitude
Introduced in 69a42c4a5c37a5a74c4b285c64328bc88ed8e059

Change-Id: I3cace297e0494bdb4da5e6d4d1a634877c66f916
Reviewed-by: Alex Blasche <alexander.blasche@qt.io>

src/location/maps/qgeomap.cpp
@@ -122,7 +122,9 @@ bool QGeoMap::setBearing(qreal bearing, const QGeoCoordinate &coordinate)
 
 bool QGeoMap::anchorCoordinateToPoint(const QGeoCoordinate &coordinate, const QPointF &anchorPoint)
 {
+    Q_D(QGeoMap);
     QGeoCoordinate newCenter = geoProjection().anchorCoordinateToPoint(coordinate, anchorPoint);
+    newCenter.setLatitude(qBound(-d->m_maximumViewportLatitude, newCenter.latitude(), d->m_maximumViewportLatitude));
     QGeoCameraData data = cameraData();
     if (data.center() != newCenter) {
         data.setCenter(newCenter);
@@ -175,7 +177,7 @@ double QGeoMap::minimumZoom() const
 double QGeoMap::maximumCenterLatitudeAtZoom(const QGeoCameraData &cameraData) const
 {
     Q_D(const QGeoMap);
-    return d->m_geoProjection->maximumCenterLatitudeAtZoom(cameraData);
+    return d->maximumCenterLatitudeAtZoom(cameraData);
 }
 
 double QGeoMap::mapWidth() const
@@ -412,6 +414,12 @@ void QGeoMapPrivate::setCopyrightVisible(bool visible)
 bool QGeoMapPrivate::copyrightVisible() const
 {
     return m_copyrightVisible;
+}
+
+double QGeoMapPrivate::maximumCenterLatitudeAtZoom(const QGeoCameraData &cameraData) const
+{
+    m_maximumViewportLatitude = m_geoProjection->maximumCenterLatitudeAtZoom(cameraData);
+    return m_maximumViewportLatitude;
 }
 
 QT_END_NAMESPACE

src/location/maps/qgeomap_p_p.h
@@ -102,6 +102,7 @@ protected:
 
     virtual void setCopyrightVisible(bool visible);
     virtual bool copyrightVisible() const;
+    virtual double maximumCenterLatitudeAtZoom(const QGeoCameraData &cameraData) const;
 
 protected:
     QSize m_viewportSize;
@@ -113,6 +114,7 @@ protected:
     QList<QDeclarativeGeoMapItemBase *> m_mapItems;
     QGeoCameraCapabilities m_cameraCapabilities;
     bool m_copyrightVisible = true;
+    mutable double m_maximumViewportLatitude = 0;
 };
 
 QT_END_NAMESPACE