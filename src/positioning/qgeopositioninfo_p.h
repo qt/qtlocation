93e6bb502dca39ccaea7f86bea05ca8040e874e2
Friedemann Kleint <Friedemann.Kleint@qt.io>
2018-03-19 09:53:56 +0100

    qgeopositioninfo_p.h: Add "We mean it" comment
    Fix warning:
    QtPositioning: WARNING: .../qtlocation/src/positioning/qgeopositioninfo_p.h does not have the "We mean it." warning
    
    Change-Id: I1f36f5505316b06b22522877bf53660fdfee79b1
    Reviewed-by: Paolo Angelelli <paolo.angelelli@qt.io>

--- src/positioning/qgeopositioninfo_p.h
+++ src/positioning/qgeopositioninfo_p.h
@@ -39,6 +39,17 @@
 #ifndef QGEOPOSITIONINFO_P_H
 #define QGEOPOSITIONINFO_P_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API. It exists purely as an
+// implementation detail. This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include <QtPositioning/private/qpositioningglobal_p.h>
 #include "qgeopositioninfo.h"
 #include <QHash>
