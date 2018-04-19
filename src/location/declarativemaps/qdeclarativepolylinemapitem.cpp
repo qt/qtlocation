qt/qtlocation

commit f0bc0db15ed262a8f31bb07b3b46a5cdf9277e79

Fix MapPolyline QQuickItem geometry to allow enabling layers
The geometry now considers the line width, so that, when enabling
layers, the item won't be incorrectly clipped at the borders.

Task-number: QTBUG-38459
Change-Id: I82c1e49c914a4a1219bb1f1ec3a70ff7bfbfbcfe
Reviewed-by: Alex Blasche <alexander.blasche@qt.io>

src/location/declarativemaps/qdeclarativepolylinemapitem.cpp
@@ -407,7 +407,7 @@ void QGeoMapPolylineGeometry::updateScreenPoints(const QGeoMap &map,
     }
 
     screenBounds_ = bb;
-    this->translate( -1 * sourceBounds_.topLeft());
+    this->translate( -1 * sourceBounds_.topLeft() + QPointF(strokeWidth, strokeWidth));
 }
 
 QDeclarativePolylineMapItem::QDeclarativePolylineMapItem(QQuickItem *parent)
@@ -763,8 +763,8 @@ void QDeclarativePolylineMapItem::updatePolish()
     geometry_.updateSourcePoints(*map(), geopathProjected_, geopath_.boundingGeoRectangle().topLeft());
     geometry_.updateScreenPoints(*map(), line_.width());
 
-    setWidth(geometry_.sourceBoundingBox().width());
-    setHeight(geometry_.sourceBoundingBox().height());
+    setWidth(geometry_.sourceBoundingBox().width() + 2 * line_.width());
+    setHeight(geometry_.sourceBoundingBox().height() + 2 * line_.width());
 
     setPositionOnMap(geometry_.origin(), -1 * geometry_.sourceBoundingBox().topLeft());
 }

commit ecc8182f6c6c8b1448a35de46c3f5598bbf35150

Fix MapPolyline being drawn misplaced and moving when clipped
Regression introduced in f0bc0db15ed262a8f31bb07b3b46a5cdf9277e79

Change-Id: I4c4a3cb2302daeb825be21f9e2f6c235d2cb7c93
Reviewed-by: Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@qt.io>

src/location/declarativemaps/qdeclarativepolylinemapitem.cpp
@@ -519,7 +519,8 @@ void QGeoMapPolylineGeometry::updateSourcePoints(const QGeoMap &map,
     \internal
 */
 void QGeoMapPolylineGeometry::updateScreenPoints(const QGeoMap &map,
-                                                 qreal strokeWidth)
+                                                 qreal strokeWidth,
+                                                 bool adjustTranslation)
 {
     if (!screenDirty_)
         return;
@@ -583,7 +584,8 @@ void QGeoMapPolylineGeometry::updateScreenPoints(const QGeoMap &map,
     }
 
     screenBounds_ = bb;
-    this->translate( -1 * sourceBounds_.topLeft() + QPointF(strokeWidth, strokeWidth));
+    const QPointF strokeOffset = (adjustTranslation) ? QPointF(strokeWidth, strokeWidth) : QPointF();
+    this->translate( -1 * sourceBounds_.topLeft() + strokeOffset);
 }
 
 void QGeoMapPolylineGeometry::clearSource()
@@ -948,7 +950,7 @@ void QDeclarativePolylineMapItem::updatePolish()
     setWidth(geometry_.sourceBoundingBox().width() + 2 * line_.width());
     setHeight(geometry_.sourceBoundingBox().height() + 2 * line_.width());
 
-    setPositionOnMap(geometry_.origin(), -1 * geometry_.sourceBoundingBox().topLeft());
+    setPositionOnMap(geometry_.origin(), -1 * geometry_.sourceBoundingBox().topLeft() + QPointF(line_.width(), line_.width()));
 }
 
 void QDeclarativePolylineMapItem::markSourceDirtyAndUpdate()

src/location/declarativemaps/qdeclarativepolylinemapitem_p.h
@@ -95,7 +95,8 @@ public:
                             const QGeoCoordinate geoLeftBound);
 
     void updateScreenPoints(const QGeoMap &map,
-                            qreal strokeWidth);
+                            qreal strokeWidth,
+                            bool adjustTranslation = true);
 
     void clearSource();
 

src/location/labs/qsg/qmapcircleobjectqsg.cpp
@@ -131,7 +131,7 @@ void QMapCircleObjectPrivateQSG::updateGeometry()
         if (clippedPaths.size()) {
             borderLeftBoundWrapped = p.geoToWrappedMapProjection(geometryOrigin);
             m_borderGeometry.pathToScreen(*m_map, clippedPaths, borderLeftBoundWrapped);
-            m_borderGeometry.updateScreenPoints(*m_map, borderWidth());
+            m_borderGeometry.updateScreenPoints(*m_map, borderWidth(), false);
         } else {
             m_borderGeometry.clear();
         }

src/location/labs/qsg/qmappolygonobjectqsg.cpp
@@ -198,7 +198,7 @@ void QMapPolygonObjectPrivateQSG::updateGeometry()
         if (clippedPaths.size()) {
             borderLeftBoundWrapped = p.geoToWrappedMapProjection(geometryOrigin);
             m_borderGeometry.pathToScreen(*m_map.data(), clippedPaths, borderLeftBoundWrapped);
-            m_borderGeometry.updateScreenPoints(*m_map.data(), borderWidth());
+            m_borderGeometry.updateScreenPoints(*m_map.data(), borderWidth(), false);
         } else {
             m_borderGeometry.clear();
         }

src/location/labs/qsg/qmappolylineobjectqsg.cpp
@@ -90,7 +90,7 @@ void QMapPolylineObjectPrivateQSG::updateGeometry()
     const QList<QDoubleVector2D> &geopathProjected = projectPath();
     m_geometry.setPreserveGeometry(true, m_geoPath.boundingGeoRectangle().topLeft());
     m_geometry.updateSourcePoints(*m_map.data(), geopathProjected, m_geoPath.boundingGeoRectangle().topLeft());
-    m_geometry.updateScreenPoints(*m_map.data(), width());
+    m_geometry.updateScreenPoints(*m_map.data(), width(), false);
 
     QPointF origin = m_map->geoProjection().coordinateToItemPosition(m_geometry.origin(), false).toPointF();
     m_geometry.translate(origin - m_geometry.firstPointOffset());