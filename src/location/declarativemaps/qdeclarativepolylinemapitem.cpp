cad3ffc8c90f3c194d06e52c981cd54cdab509ec
Paolo Angelelli <paolo.angelelli@qt.io>
2018-05-08 13:04:41 +0200

    Fix MapPolyline doc
    GL_LINES aren't used anymore. Remove the related note.
    
    Task-number: QTBUG-63223
    Change-Id: Ic309b3ad4626506151b20f69e5f11a3f1d7cc888
    Reviewed-by: Alex Blasche <alexander.blasche@qt.io>

--- src/location/declarativemaps/qdeclarativepolylinemapitem.cpp
+++ src/location/declarativemaps/qdeclarativepolylinemapitem.cpp
@@ -272,11 +272,6 @@ static QList<QList<QDoubleVector2D> > clipLine(
     appearance. Setting the \l {Item::opacity}{opacity} property will force the object to
     be blended, which decreases performance considerably depending on the hardware in use.
 
-    \note MapPolylines are implemented using the OpenGL GL_LINES
-    primitive. There have been occasional reports of issues and rendering
-    inconsistencies on some (particularly quite old) platforms. No workaround
-    is yet available for these issues.
-
     \section2 Example Usage
 
     The following snippet shows a MapPolyline with 4 points, making a shape
