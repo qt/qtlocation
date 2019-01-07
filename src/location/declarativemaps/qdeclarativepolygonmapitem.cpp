qt/qtlocation

commit e7bb8f636086c04acd97e4eb3c42e7c6c05dc8f2

QGeoPath/QGeoPolygon: defer bbox computation
This patch introduces 2 versions of QGeoPath/polygon private:
a lazy version (default) and an eager version.
The reason is that certain classes such as MapItems make heavy use
of the bounding box of the geoshapes, as well as the contains
method, and in those cases it's beneficial to have it eagerly
computed and cached.

Other use cases do not see this feature so much in use, and the added
costs, both in terms of computation and in terms of memory requirements
for cached data can be avoided.

As the patch currently stands, using copy constructors for
QGeoPath and QGeoPolygon with a QGeoPathEager and a QGeoPolygonEager
(and vice-versa) changes the type of d_ptr.
This means that doing, for example, QGeoPath(someQGeoPathEager)
effectively returns a QGeoPath that behaves like a QGeoPathEager
(although not being one).

Change-Id: I8cfed1e0a747139d0fb6d5fb5236bf5f5fbf24c1
Reviewed-by: Alex Blasche <alexander.blasche@qt.io>

src/location/declarativemaps/qdeclarativepolygonmapitem.cpp
@@ -50,6 +50,7 @@
 
 #include <QtPositioning/private/qdoublevector2d_p.h>
 #include <QtPositioning/private/qclipperutils_p.h>
+#include <QtPositioning/private/qgeopolygon_p.h>
 
 /* poly2tri triangulator includes */
 #include <clip2tri.h>
@@ -318,6 +319,7 @@ QDeclarativePolygonMapItem::QDeclarativePolygonMapItem(QQuickItem *parent)
 :   QDeclarativeGeoMapItemBase(parent), border_(this), color_(Qt::transparent), dirtyMaterial_(true),
     updatingGeometry_(false)
 {
+    geopath_ = QGeoPolygonEager();
     setFlag(ItemHasContents, true);
     QObject::connect(&border_, SIGNAL(colorChanged(QColor)),
                      this, SLOT(markSourceDirtyAndUpdate()));
@@ -611,7 +613,7 @@ void QDeclarativePolygonMapItem::setGeoShape(const QGeoShape &shape)
     if (shape == geopath_)
         return;
 
-    geopath_ = shape;
+    geopath_ = QGeoPathEager(shape);
     regenerateCache();
     geometry_.setPreserveGeometry(true, geopath_.boundingGeoRectangle().topLeft());
     borderGeometry_.setPreserveGeometry(true, geopath_.boundingGeoRectangle().topLeft());

src/location/declarativemaps/qdeclarativepolylinemapitem.cpp
@@ -54,6 +54,7 @@
 #include <QtGui/private/qtriangulator_p.h>
 
 #include <QtPositioning/private/qclipperutils_p.h>
+#include <QtPositioning/private/qgeopath_p.h>
 #include <array>
 
 QT_BEGIN_NAMESPACE
@@ -738,6 +739,7 @@ bool QGeoMapPolylineGeometry::contains(const QPointF &point) const
 QDeclarativePolylineMapItem::QDeclarativePolylineMapItem(QQuickItem *parent)
 :   QDeclarativeGeoMapItemBase(parent), line_(this), dirtyMaterial_(true), updatingGeometry_(false)
 {
+    geopath_ = QGeoPathEager();
     setFlag(ItemHasContents, true);
     QObject::connect(&line_, SIGNAL(colorChanged(QColor)),
                      this, SLOT(updateAfterLinePropertiesChanged()));
@@ -806,7 +808,7 @@ void QDeclarativePolylineMapItem::setPath(const QGeoPath &path)
     if (geopath_.path() == path.path())
         return;
 
-    geopath_ = path;
+    geopath_ = QGeoPathEager(path);
     regenerateCache();
     geometry_.setPreserveGeometry(true, geopath_.boundingGeoRectangle().topLeft());
     markSourceDirtyAndUpdate();
@@ -1135,18 +1137,8 @@ const QGeoShape &QDeclarativePolylineMapItem::geoShape() const
 
 void QDeclarativePolylineMapItem::setGeoShape(const QGeoShape &shape)
 {
-    if (shape == geopath_)
-        return;
-
     const QGeoPath geopath(shape); // if shape isn't a path, path will be created as a default-constructed path
-    const bool pathHasChanged = geopath.path() != geopath_.path();
-    geopath_ = geopath;
-
-    regenerateCache();
-    geometry_.setPreserveGeometry(true, geopath_.boundingGeoRectangle().topLeft());
-    markSourceDirtyAndUpdate();
-    if (pathHasChanged)
-        emit pathChanged();
+    setPath(geopath);
 }
 
 QGeoMap::ItemType QDeclarativePolylineMapItem::itemType() const

src/positioning/positioning.pro
@@ -56,6 +56,7 @@ PRIVATE_HEADERS += \
                     qlocationdata_simulator_p.h \
                     qdoublematrix4x4_p.h \
                     qgeopath_p.h \
+                    qgeopolygon_p.h \
                     qgeocoordinateobject_p.h \
                     qgeopositioninfo_p.h \
                     qclipperutils_p.h

src/positioning/qgeopath.cpp
@@ -114,7 +114,7 @@ Q_GLOBAL_STATIC(PathVariantConversions, initPathConversions)
     Constructs a new, empty geo path.
 */
 QGeoPath::QGeoPath()
-:   QGeoShape(new QGeoPathPrivate(QGeoShape::PathType))
+:   QGeoShape(new QGeoPathPrivate())
 {
     initPathConversions();
 }
@@ -124,7 +124,7 @@ QGeoPath::QGeoPath()
     (\a path and \a width).
 */
 QGeoPath::QGeoPath(const QList<QGeoCoordinate> &path, const qreal &width)
-:   QGeoShape(new QGeoPathPrivate(QGeoShape::PathType, path, width))
+:   QGeoShape(new QGeoPathPrivate(path, width))
 {
     initPathConversions();
 }
@@ -146,7 +146,7 @@ QGeoPath::QGeoPath(const QGeoShape &other)
 {
     initPathConversions();
     if (type() != QGeoShape::PathType)
-        d_ptr = new QGeoPathPrivate(QGeoShape::PathType);
+        d_ptr = new QGeoPathPrivate();
 }
 
 /*!
@@ -391,33 +391,54 @@ QString QGeoPath::toString() const
 }
 
 /*******************************************************************************
- * QGeoPathPrivate
+ *
+ * QGeoPathPrivate & friends
+ *
 *******************************************************************************/
 
-QGeoPathPrivate::QGeoPathPrivate(QGeoShape::ShapeType type)
-:   QGeoShapePrivate(type), m_width(0), m_clipperDirty(true)
+QGeoPathPrivate::QGeoPathPrivate()
+:   QGeoShapePrivate(QGeoShape::PathType)
 {
+
 }
 
-QGeoPathPrivate::QGeoPathPrivate(QGeoShape::ShapeType type, const QList<QGeoCoordinate> &path, const qreal width)
-:   QGeoShapePrivate(type), m_width(0), m_clipperDirty(true)
+QGeoPathPrivate::QGeoPathPrivate(const QList<QGeoCoordinate> &path, const qreal width)
+:   QGeoShapePrivate(QGeoShape::PathType)
 {
     setPath(path);
     setWidth(width);
 }
 
-QGeoPathPrivate::QGeoPathPrivate(const QGeoPathPrivate &other)
-:   QGeoShapePrivate(other.type), m_path(other.m_path),
-    m_deltaXs(other.m_deltaXs), m_minX(other.m_minX), m_maxX(other.m_maxX), m_minLati(other.m_minLati),
-    m_maxLati(other.m_maxLati), m_bbox(other.m_bbox), m_width(other.m_width), m_clipperDirty(true)
+QGeoPathPrivate::~QGeoPathPrivate()
 {
-}
 
-QGeoPathPrivate::~QGeoPathPrivate() {}
+}
 
 QGeoShapePrivate *QGeoPathPrivate::clone() const
 {
     return new QGeoPathPrivate(*this);
+}
+
+bool QGeoPathPrivate::isValid() const
+{
+    return !isEmpty();
+}
+
+bool QGeoPathPrivate::isEmpty() const
+{
+    return path().isEmpty(); // this should perhaps return geometric emptiness, less than 2 points for line, or empty polygon for polygons
+}
+
+QGeoCoordinate QGeoPathPrivate::center() const
+{
+    return boundingGeoRectangle().center();
+}
+
+void QGeoPathPrivate::extendShape(const QGeoCoordinate &coordinate)
+{
+    if (!coordinate.isValid() || contains(coordinate))
+        return;
+    addCoordinate(coordinate);
 }
 
 bool QGeoPathPrivate::operator==(const QGeoShapePrivate &other) const
@@ -428,91 +449,12 @@ bool QGeoPathPrivate::operator==(const QGeoShapePrivate &other) const
     const QGeoPathPrivate &otherPath = static_cast<const QGeoPathPrivate &>(other);
     if (m_path.size() != otherPath.m_path.size())
         return false;
-
-    if (type == QGeoShape::PathType)
-        return m_width == otherPath.m_width && m_path == otherPath.m_path;
-    else
-        return m_path == otherPath.m_path && m_holesList == otherPath.m_holesList;
-}
-
-bool QGeoPathPrivate::isValid() const
-{
-    if (type == QGeoShape::PathType)
-        return !isEmpty();
-    else
-        return m_path.size() > 2;
-
-}
-
-bool QGeoPathPrivate::isEmpty() const
-{
-    return m_path.isEmpty(); // this should perhaps return geometric emptiness, less than 2 points for line, or empty polygon for polygons
+    return m_width == otherPath.m_width && m_path == otherPath.m_path;
 }
 
 const QList<QGeoCoordinate> &QGeoPathPrivate::path() const
 {
     return m_path;
-}
-
-void QGeoPathPrivate::setPath(const QList<QGeoCoordinate> &path)
-{
-    for (const QGeoCoordinate &c: path)
-        if (!c.isValid())
-            return;
-    m_path = path;
-    computeBoundingBox();
-}
-
-void QGeoPathPrivate::clearPath()
-{
-    m_path.clear();
-    computeBoundingBox();
-}
-
-qreal QGeoPathPrivate::width() const
-{
-    return m_width;
-}
-
-void QGeoPathPrivate::setWidth(const qreal &width)
-{
-    if (qIsNaN(width) || width < 0.0)
-        return;
-    m_width = width;
-}
-
-double QGeoPathPrivate::length(int indexFrom, int indexTo) const
-{
-    if (path().isEmpty())
-        return 0.0;
-
-    bool wrap = indexTo == -1;
-    if (indexTo < 0 || indexTo >= path().size())
-        indexTo = path().size() - 1;
-    double len = 0.0;
-    // TODO: consider calculating the length of the actual rhumb line segments
-    // instead of the shortest path from A to B.
-    for (int i = indexFrom; i < indexTo; i++)
-        len += m_path[i].distanceTo(m_path[i+1]);
-    if (wrap)
-        len += m_path.last().distanceTo(m_path.first());
-    return len;
-}
-
-int QGeoPathPrivate::size() const
-{
-    return m_path.size();
-}
-
-/*!
-    Returns true if coordinate is present in m_path.
-*/
-bool QGeoPathPrivate::contains(const QGeoCoordinate &coordinate) const
-{
-    if (type == QGeoShape::PathType)
-        return lineContains(coordinate);
-    else
-        return polygonContains(coordinate);
 }
 
 bool QGeoPathPrivate::lineContains(const QGeoCoordinate &coordinate) const
@@ -584,99 +526,44 @@ bool QGeoPathPrivate::lineContains(const QGeoCoordinate &coordinate) const
     return (m_path[0].distanceTo(coordinate) <= lineRadius);
 }
 
-/*!
-    modified version of polygonContains with holes support.
-*/
-bool QGeoPathPrivate::polygonContains(const QGeoCoordinate &coordinate) const
+bool QGeoPathPrivate::contains(const QGeoCoordinate &coordinate) const
 {
-    if (m_clipperDirty)
-        const_cast<QGeoPathPrivate *>(this)->updateClipperPath();
-
-    // iterates the holes List checking whether the point is contained inside the holes
-    for (const QList<QGeoCoordinate> &holePath : qAsConst(m_holesList)) {
-
-        QGeoPolygon holePolygon;
-        holePolygon.setPath(holePath);
-        QGeoPath holeBoundary;
-        holeBoundary.setPath(holePath);
-
-        if (holePolygon.contains(coordinate) && !(holeBoundary.contains(coordinate)))
-            return false;
-    }
-
-    QDoubleVector2D coord = QWebMercator::coordToMercator(coordinate);
-    double tlx = QWebMercator::coordToMercator(m_bbox.topLeft()).x();
-    if (coord.x() < tlx)
-        coord.setX(coord.x() + 1.0);
-
-    IntPoint intCoord = QClipperUtils::toIntPoint(coord);
-    return c2t::clip2tri::pointInPolygon(intCoord, m_clipperPath) != 0;
+    return lineContains(coordinate);
 }
 
-QGeoCoordinate QGeoPathPrivate::center() const
+qreal QGeoPathPrivate::width() const
 {
-    return boundingGeoRectangle().center();
+    return m_width;
 }
 
-QGeoRectangle QGeoPathPrivate::boundingGeoRectangle() const
+void QGeoPathPrivate::setWidth(const qreal &width)
 {
-    return m_bbox;
-}
-
-void QGeoPathPrivate::extendShape(const QGeoCoordinate &coordinate)
-{
-    if (!coordinate.isValid() || contains(coordinate))
+    if (qIsNaN(width) || width < 0.0)
         return;
-    addCoordinate(coordinate);
+    m_width = width;
 }
 
-void QGeoPathPrivate::translate(double degreesLatitude, double degreesLongitude)
+double QGeoPathPrivate::length(int indexFrom, int indexTo) const
 {
-    if (degreesLatitude > 0.0)
-        degreesLatitude = qMin(degreesLatitude, 90.0 - m_maxLati);
-    else
-        degreesLatitude = qMax(degreesLatitude, -90.0 - m_minLati);
-    for (QGeoCoordinate &p: m_path) {
-        p.setLatitude(p.latitude() + degreesLatitude);
-        p.setLongitude(QLocationUtils::wrapLong(p.longitude() + degreesLongitude));
-    }
-    if (!m_holesList.isEmpty()){
-        for (QList<QGeoCoordinate> &hole: m_holesList){
-            for (QGeoCoordinate &holeVertex: hole){
-                holeVertex.setLatitude(holeVertex.latitude() + degreesLatitude);
-                holeVertex.setLongitude(QLocationUtils::wrapLong(holeVertex.longitude() + degreesLongitude));
-            }
-        }
-    }
-    m_bbox.translate(degreesLatitude, degreesLongitude);
-    m_minLati += degreesLatitude;
-    m_maxLati += degreesLatitude;
+    if (path().isEmpty())
+        return 0.0;
+
+    bool wrap = indexTo == -1;
+    if (indexTo < 0 || indexTo >= path().size())
+        indexTo = path().size() - 1;
+    double len = 0.0;
+    // TODO: consider calculating the length of the actual rhumb line segments
+    // instead of the shortest path from A to B.
+    for (int i = indexFrom; i < indexTo; i++)
+        len += m_path[i].distanceTo(m_path[i+1]);
+    if (wrap)
+        len += m_path.last().distanceTo(m_path.first());
+    return len;
 }
 
-void QGeoPathPrivate::addCoordinate(const QGeoCoordinate &coordinate)
+int QGeoPathPrivate::size() const
 {
-    if (!coordinate.isValid())
-        return;
-    m_path.append(coordinate);
-    updateBoundingBox();
-}
-
-void QGeoPathPrivate::insertCoordinate(int index, const QGeoCoordinate &coordinate)
-{
-    if (index < 0 || index > m_path.size() || !coordinate.isValid())
-        return;
-
-    m_path.insert(index, coordinate);
-    computeBoundingBox();
-}
-
-void QGeoPathPrivate::replaceCoordinate(int index, const QGeoCoordinate &coordinate)
-{
-    if (index < 0 || index >= m_path.size() || !coordinate.isValid())
-        return;
-
-    m_path[index] = coordinate;
-    computeBoundingBox();
+    return m_path.size();
 }
 
 QGeoCoordinate QGeoPathPrivate::coordinateAt(int index) const
@@ -692,6 +579,71 @@ bool QGeoPathPrivate::containsCoordinate(const QGeoCoordinate &coordinate) const
     return m_path.indexOf(coordinate) > -1;
 }
 
+void QGeoPathPrivate::translate(double degreesLatitude, double degreesLongitude)
+{
+    // Need min/maxLati, so update bbox
+    QVector<double> m_deltaXs;
+    double m_minX, m_maxX, m_minLati, m_maxLati;
+    m_bboxDirty = false;
+    computeBBox(m_path, m_deltaXs, m_minX, m_maxX, m_minLati, m_maxLati, m_bbox);
+
+    if (degreesLatitude > 0.0)
+        degreesLatitude = qMin(degreesLatitude, 90.0 - m_maxLati);
+    else
+        degreesLatitude = qMax(degreesLatitude, -90.0 - m_minLati);
+    for (QGeoCoordinate &p: m_path) {
+        p.setLatitude(p.latitude() + degreesLatitude);
+        p.setLongitude(QLocationUtils::wrapLong(p.longitude() + degreesLongitude));
+    }
+    m_bbox.translate(degreesLatitude, degreesLongitude);
+}
+
+QGeoRectangle QGeoPathPrivate::boundingGeoRectangle() const
+{
+    if (m_bboxDirty)
+        const_cast<QGeoPathPrivate &>(*this).computeBoundingBox();
+    return m_bbox;
+}
+
+void QGeoPathPrivate::setPath(const QList<QGeoCoordinate> &path)
+{
+    for (const QGeoCoordinate &c: path)
+        if (!c.isValid())
+            return;
+    m_path = path;
+    markDirty();
+}
+
+void QGeoPathPrivate::clearPath()
+{
+    m_path.clear();
+    markDirty();
+}
+
+void QGeoPathPrivate::addCoordinate(const QGeoCoordinate &coordinate)
+{
+    if (!coordinate.isValid())
+        return;
+    m_path.append(coordinate);
+    markDirty();
+}
+
+void QGeoPathPrivate::insertCoordinate(int index, const QGeoCoordinate &coordinate)
+{
+    if (index < 0 || index > m_path.size() || !coordinate.isValid())
+        return;
+    m_path.insert(index, coordinate);
+    markDirty();
+}
+
+void QGeoPathPrivate::replaceCoordinate(int index, const QGeoCoordinate &coordinate)
+{
+    if (index < 0 || index >= m_path.size() || !coordinate.isValid())
+        return;
+    m_path[index] = coordinate;
+    markDirty();
+}
+
 void QGeoPathPrivate::removeCoordinate(const QGeoCoordinate &coordinate)
 {
     int index = m_path.lastIndexOf(coordinate);
@@ -702,171 +654,121 @@ void QGeoPathPrivate::removeCoordinate(int index)
 {
     if (index < 0 || index >= m_path.size())
         return;
-
     m_path.removeAt(index);
-    computeBoundingBox();
+    markDirty();
+}
+
+void QGeoPathPrivate::markDirty()
+{
+    m_bboxDirty = true;
 }
 
 void QGeoPathPrivate::computeBoundingBox()
 {
-    m_clipperDirty = true;
-    if (m_path.isEmpty()) {
-        m_deltaXs.clear();
-        m_minX = qInf();
-        m_maxX = -qInf();
-        m_minLati = qInf();
-        m_maxLati = -qInf();
-        m_bbox = QGeoRectangle();
+    QVector<double> m_deltaXs;
+    double m_minX, m_maxX, m_minLati, m_maxLati;
+    m_bboxDirty = false;
+    computeBBox(m_path, m_deltaXs, m_minX, m_maxX, m_minLati, m_maxLati, m_bbox);
+}
+
+QGeoPathPrivateEager::QGeoPathPrivateEager()
+:   QGeoPathPrivate()
+{
+    m_bboxDirty = false; // never dirty on the eager version
+}
+
+QGeoPathPrivateEager::QGeoPathPrivateEager(const QList<QGeoCoordinate> &path, const qreal width)
+:   QGeoPathPrivate(path, width)
+{
+    m_bboxDirty = false; // never dirty on the eager version
+}
+
+QGeoPathPrivateEager::~QGeoPathPrivateEager()
+{
+
+}
+
+QGeoShapePrivate *QGeoPathPrivateEager::clone() const
+{
+    return new QGeoPathPrivateEager(*this);
+}
+
+void QGeoPathPrivateEager::markDirty()
+{
+    computeBoundingBox();
+}
+
+void QGeoPathPrivateEager::translate(double degreesLatitude, double degreesLongitude)
+{
+    if (degreesLatitude > 0.0)
+        degreesLatitude = qMin(degreesLatitude, 90.0 - m_maxLati);
+    else
+        degreesLatitude = qMax(degreesLatitude, -90.0 - m_minLati);
+    for (QGeoCoordinate &p: m_path) {
+        p.setLatitude(p.latitude() + degreesLatitude);
+        p.setLongitude(QLocationUtils::wrapLong(p.longitude() + degreesLongitude));
+    }
+    m_bbox.translate(degreesLatitude, degreesLongitude);
+    m_minLati += degreesLatitude;
+    m_maxLati += degreesLatitude;
+}
+
+void QGeoPathPrivateEager::addCoordinate(const QGeoCoordinate &coordinate)
+{
+    if (!coordinate.isValid())
         return;
-    }
-
-    m_minLati = m_maxLati = m_path.at(0).latitude();
-    int minId = 0;
-    int maxId = 0;
-    m_deltaXs.resize(m_path.size());
-    m_deltaXs[0] = m_minX = m_maxX = 0.0;
-
-    for (int i = 1; i < m_path.size(); i++) {
-        const QGeoCoordinate &geoFrom = m_path.at(i-1);
-        const QGeoCoordinate &geoTo   = m_path.at(i);
-        double longiFrom    = geoFrom.longitude();
-        double longiTo      = geoTo.longitude();
-        double deltaLongi = longiTo - longiFrom;
-        if (qAbs(deltaLongi) > 180.0) {
-            if (longiTo > 0.0)
-                longiTo -= 360.0;
-            else
-                longiTo += 360.0;
-            deltaLongi =  longiTo - longiFrom;
-        }
-        m_deltaXs[i] = m_deltaXs[i-1] + deltaLongi;
-        if (m_deltaXs[i] < m_minX) {
-            m_minX = m_deltaXs[i];
-            minId = i;
-        }
-        if (m_deltaXs[i] > m_maxX) {
-            m_maxX = m_deltaXs[i];
-            maxId = i;
-        }
-        if (geoTo.latitude() > m_maxLati)
-            m_maxLati = geoTo.latitude();
-        if (geoTo.latitude() < m_minLati)
-            m_minLati = geoTo.latitude();
-    }
-
-    m_bbox = QGeoRectangle(QGeoCoordinate(m_maxLati, m_path.at(minId).longitude()),
-                           QGeoCoordinate(m_minLati, m_path.at(maxId).longitude()));
+    m_path.append(coordinate);
+    //m_clipperDirty = true; // clipper not used in polylines
+    updateBoundingBox();
 }
 
-void QGeoPathPrivate::updateBoundingBox()
+void QGeoPathPrivateEager::QGeoPathPrivateEager::computeBoundingBox()
 {
-    m_clipperDirty = true;
-    if (m_path.isEmpty()) {
-        m_deltaXs.clear();
-        m_minX = qInf();
-        m_maxX = -qInf();
-        m_minLati = qInf();
-        m_maxLati = -qInf();
-        m_bbox = QGeoRectangle();
-        return;
-    } else if (m_path.size() == 1) { // was 0  now is 1
-        m_deltaXs.resize(1);
-        m_deltaXs[0] = m_minX = m_maxX = 0.0;
-        m_minLati = m_maxLati = m_path.at(0).latitude();
-        m_bbox = QGeoRectangle(QGeoCoordinate(m_maxLati, m_path.at(0).longitude()),
-                               QGeoCoordinate(m_minLati, m_path.at(0).longitude()));
-        return;
-    } else if ( m_path.size() != m_deltaXs.size() + 1 ) {  // this case should not happen
-        computeBoundingBox(); // something went wrong
-        return;
-    }
-
-    const QGeoCoordinate &geoFrom = m_path.at(m_path.size()-2);
-    const QGeoCoordinate &geoTo   = m_path.last();
-    double longiFrom    = geoFrom.longitude();
-    double longiTo      = geoTo.longitude();
-    double deltaLongi = longiTo - longiFrom;
-    if (qAbs(deltaLongi) > 180.0) {
-        if (longiTo > 0.0)
-            longiTo -= 360.0;
-        else
-            longiTo += 360.0;
-        deltaLongi =  longiTo - longiFrom;
-    }
-
-    m_deltaXs.push_back(m_deltaXs.last() + deltaLongi);
-    double currentMinLongi = m_bbox.topLeft().longitude();
-    double currentMaxLongi = m_bbox.bottomRight().longitude();
-    if (m_deltaXs.last() < m_minX) {
-        m_minX = m_deltaXs.last();
-        currentMinLongi = geoTo.longitude();
-    }
-    if (m_deltaXs.last() > m_maxX) {
-        m_maxX = m_deltaXs.last();
-        currentMaxLongi = geoTo.longitude();
-    }
-    if (geoTo.latitude() > m_maxLati)
-        m_maxLati = geoTo.latitude();
-    if (geoTo.latitude() < m_minLati)
-        m_minLati = geoTo.latitude();
-    m_bbox = QGeoRectangle(QGeoCoordinate(m_maxLati, currentMinLongi),
-                           QGeoCoordinate(m_minLati, currentMaxLongi));
+    computeBBox(m_path, m_deltaXs, m_minX, m_maxX, m_minLati, m_maxLati, m_bbox);
 }
 
-void QGeoPathPrivate::updateClipperPath()
+void QGeoPathPrivateEager::QGeoPathPrivateEager::updateBoundingBox()
 {
-    m_clipperDirty = false;
-    double tlx = QWebMercator::coordToMercator(m_bbox.topLeft()).x();
-    QList<QDoubleVector2D> preservedPath;
-    for (const QGeoCoordinate &c : m_path) {
-        QDoubleVector2D crd = QWebMercator::coordToMercator(c);
-        if (crd.x() < tlx)
-            crd.setX(crd.x() + 1.0);
-        preservedPath << crd;
-    }
-    m_clipperPath = QClipperUtils::qListToPath(preservedPath);
+    updateBBox(m_path, m_deltaXs, m_minX, m_maxX, m_minLati, m_maxLati, m_bbox);
 }
 
-
-/*!
-    Sets the \a path for an Hole inside the polygon.The hole has QList<QGeoCoordinate> type
-*/
-void QGeoPathPrivate::addHole(const QList<QGeoCoordinate> &holePath)
+QGeoPathEager::QGeoPathEager() : QGeoPath()
 {
-    for (const QGeoCoordinate &holeVertex: holePath)
-        if (!holeVertex.isValid())
-            return;
-
-    m_holesList << holePath;
+    initPathConversions();
+    d_ptr = new QGeoPathPrivateEager;
 }
 
-/*!
-    Returns a QVariant containing a QList<QGeoCoordinate> representing the hole at index
-*/
-const QList<QGeoCoordinate> QGeoPathPrivate::holePath(int index) const
+QGeoPathEager::QGeoPathEager(const QList<QGeoCoordinate> &path, const qreal &width) : QGeoPath()
 {
-    return m_holesList.at(index);
+    initPathConversions();
+    d_ptr = new QGeoPathPrivateEager(path, width);
 }
 
-
-/*!
-    Removes element at position \a index from the holes QList.
-*/
-void QGeoPathPrivate::removeHole(int index)
+QGeoPathEager::QGeoPathEager(const QGeoPath &other) : QGeoPath()
 {
-    if (index < 0 || index >= m_holesList.size())
-        return;
-
-    m_holesList.removeAt(index);
+    initPathConversions();
+    d_ptr = new QGeoPathPrivateEager;
+    setPath(other.path());
+    setWidth(other.width());
 }
 
-/*!
-    Returns the number of holes.
-*/
-int QGeoPathPrivate::holesCount() const
+QGeoPathEager::QGeoPathEager(const QGeoShape &other) : QGeoPath()
 {
-    return m_holesList.size();
+    initPathConversions();
+    if (other.type() == QGeoShape::PathType)
+        *this = QGeoPathEager(QGeoPath(other));
+    else
+        d_ptr = new QGeoPathPrivateEager;
 }
+
+QGeoPathEager::~QGeoPathEager() {}
 
 QT_END_NAMESPACE
+
+
+
+
+
+
+
 

commit c134f72060607be6caa622200c8930211a6e7e91

Fix using the wrong type in MapPolygon setGeoShape
This patch fixes a bug introduced in
e7bb8f636086c04acd97e4eb3c42e7c6c05dc8f2 where the wrong type is used
in QDeclarativePolygonMapItem::setGeoShape

Change-Id: Ic5206bec4d499759f23abe8749c05d3df11f5ede
Reviewed-by: Alex Blasche <alexander.blasche@qt.io>

src/location/declarativemaps/qdeclarativepolygonmapitem.cpp
@@ -613,7 +613,7 @@ void QDeclarativePolygonMapItem::setGeoShape(const QGeoShape &shape)
     if (shape == geopath_)
         return;
 
-    geopath_ = QGeoPathEager(shape);
+    geopath_ = QGeoPolygonEager(shape);
     regenerateCache();
     geometry_.setPreserveGeometry(true, geopath_.boundingGeoRectangle().topLeft());
     borderGeometry_.setPreserveGeometry(true, geopath_.boundingGeoRectangle().topLeft());