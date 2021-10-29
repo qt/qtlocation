e40cd29cff975a5d564de87e1a6f0c7341bcdbd5
Ivan Solovev <ivan.solovev@qt.io>
2021-10-29 18:23:27 +0300

    Finalize QtLocation CMake build
    Add missing files from declarativemaps and declarativeplaces.
    
    Reintroduce Qt.labs.location feature (currently always enabled, as
    I'm not sure that OpenGL is strictly required for now).
    Build Qt.location.labs as part of QtLocation lib when this feature
    is enabled.
    Also update build files for plugins to take this feature into account.
    
    Build QML plugins as two separate QML modules. This still requires
    some improvements because the code does not use QML_* macros, but
    an old-style qmlRegister* approach.
    
    Task-number: QTBUG-96795
    Change-Id: Ib835848ca3ad551cc6d858d2bae10b9aa4df5160
    Reviewed-by: Andreas Buhr <andreas.buhr@qt.io>
    Reviewed-by: Alexey Edelev <alexey.edelev@qt.io>

---
+++ src/configure.cmake
@@ -0,0 +1,11 @@
+# TODO: figure out if we need any condition for that.
+# Previously was: "condition": "config.opengl"
+qt_feature("location-labs-plugin" PRIVATE
+    LABEL "Provides experimental QtLocation QML types"
+    CONDITION TRUE
+)
+
+qt_feature("geoservices_osm" PRIVATE
+    LABEL "Provides access to OpenStreetMap geoservices"
+    CONDITION QT_FEATURE_concurrent
+)
