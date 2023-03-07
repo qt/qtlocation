e45c7aefe72b58909917b0cada8c56432c84e5ee
Friedemann Kleint <Friedemann.Kleint@qt.io>
2023-03-07 14:12:42 +0100

    map viewer/places examples: Brush up the code
    - Use modern string literals
    - Use the variadic template version of QMetaObject::invokeMethod()
    - Use modern connect syntax
    - Reorder includes
    - Trim newlines in QML
    
    Fix minor bugs:
    - Skip empty elements of library path (namely when variable is not
      set)
    - Fix the check for osm.useragent to operate on the map
    - Exit cleanly on QML loading errors
    
    Since places uses similar code, apply the changes there as well.
    
    Change-Id: Ibaeffc6e003c55c458137458c5017584e0c072f1
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit 20b0c2fb20f3c37c2549b7aa59f686a7726a2817)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- examples/location/places/main.cpp
+++ examples/location/places/main.cpp
@@ -1,71 +1,68 @@
 // Copyright (C) 2017 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 
-#include <QtCore/QTextStream>
-#include <QtGui/QGuiApplication>
 #include <QtQml/QQmlApplicationEngine>
 #include <QtQml/QQmlContext>
-#include <QtQuick/QQuickItem>
 
-static bool parseArgs(QStringList& args, QVariantMap& parameters)
+#include <QtGui/QGuiApplication>
+
+#include <iostream>
+
+using namespace Qt::StringLiterals;
+
+static const char *help = R"(Usage:
+--plugin.<parameter_name> <parameter_value>    -  Sets parameter = value for plugin
+)";
+
+static QVariantMap parseArgs(QStringList args)
 {
-
+    QVariantMap parameters;
     while (!args.isEmpty()) {
-
         QString param = args.takeFirst();
-
-        if (param.startsWith("--help")) {
-            QTextStream out(stdout);
-            out << "Usage: " << Qt::endl;
-            out << "--plugin.<parameter_name> <parameter_value>    -  Sets parameter = value for plugin" << Qt::endl;
-            out.flush();
-            return true;
-        }
-
-        if (param.startsWith("--plugin.")) {
-
+        if (param.startsWith(u"--plugin.")) {
             param.remove(0, 9);
-
-            if (args.isEmpty() || args.first().startsWith("--")) {
-                parameters[param] = true;
+            if (args.isEmpty() || args.constFirst().startsWith(u"--")) {
+                parameters.insert(param, QVariant(true));
             } else {
-
                 QString value = args.takeFirst();
-
-                if (value == "true" || value == "on" || value == "enabled") {
-                    parameters[param] = true;
-                } else if (value == "false" || value == "off"
-                           || value == "disable") {
-                    parameters[param] = false;
+                if (value == u"true" || value == u"on" || value == u"enabled") {
+                    parameters.insert(param, QVariant(true));
+                } else if (value == u"false" || value == u"off"
+                           || value == u"disable") {
+                    parameters.insert(param, QVariant(false));
                 } else {
-                    parameters[param] = value;
+                    parameters.insert(param, QVariant(value));
                 }
             }
         }
     }
-    return false;
+    return parameters;
 }
 
 int main(int argc, char *argv[])
 {
     QGuiApplication application(argc, argv);
 
-    QVariantMap parameters;
-    QStringList args(QCoreApplication::arguments());
-
-    if (parseArgs(args, parameters))
+    QStringList args = QCoreApplication::arguments();
+    args.removeFirst();
+    if (args.contains(u"--help")) {
+        std::cout << qPrintable(QCoreApplication::applicationName()) << "\n\n" << help;
         return 0;
+    }
+
+    QVariantMap parameters = parseArgs(args);
 
     QQmlApplicationEngine engine;
-    engine.addImportPath(QStringLiteral(":/imports"));
-    engine.load(QUrl(QStringLiteral("qrc:///places.qml")));
-    QObject::connect(&engine, SIGNAL(quit()), qApp, SLOT(quit()));
+    engine.addImportPath(u":/imports"_s);
+    engine.load(QUrl(u"qrc:///places.qml"_s));
+    QObject::connect(&engine, &QQmlApplicationEngine::quit,
+                     qApp, QCoreApplication::quit);
 
-    QObject *item = engine.rootObjects().first();
-    Q_ASSERT(item);
+    auto *item = engine.rootObjects().value(0);
+    if (item == nullptr)
+        return -1;
 
-    QMetaObject::invokeMethod(item, "initializeProviders",
-                                 Q_ARG(QVariant, QVariant::fromValue(parameters)));
+    QMetaObject::invokeMethod(item, "initializeProviders", QVariant::fromValue(parameters));
 
     return application.exec();
 }
