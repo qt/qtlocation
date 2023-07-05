c4a84e888293557ea8d0392595f9b3fbb73108ba
Kai Köhne <kai.koehne@qt.io>
2023-07-05 15:29:49 +0200

    Doc: Don't use versioned Qt imports in documentation
    We don't promote versioned imports anymore in Qt 6.
    
    Pick-to: 6.5 6.6
    Change-Id: I29acd1f3db4c62d934df7ced47e7538dfb0a2849
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>

--- src/location/quickmapitems/qdeclarativegeomapitemgroup.cpp
+++ src/location/quickmapitems/qdeclarativegeomapitemgroup.cpp
@@ -30,9 +30,9 @@ QT_BEGIN_NAMESPACE
     This group is defined in a separate file named PolygonGroup.qml:
 
     \code
-    import QtQuick 2.4
-    import QtPositioning 5.6
-    import QtLocation 5.9
+    import QtQuick
+    import QtPositioning
+    import QtLocation
 
     MapItemGroup {
         id: itemGroup
