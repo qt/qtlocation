e105e58d88397c3fe265e4669338f57f87facb43
Lucie Gérard <lucie.gerard@qt.io>
2024-03-15 15:23:35 +0100

    Change license for test files
    According to QUIP-18 [1], all test files should be
    LicenseRef-Qt-Commercial OR GPL-3.0-only
    
    [1]: https://contribute.qt-project.org/quips/18
    
    Pick-to: 6.7
    Task-number: QTBUG-121787
    Change-Id: I3457ec28d6b3f600ce9e0ba79e23919b1dc539b4
    Reviewed-by: Kai Köhne <kai.koehne@qt.io>

--- tests/auto/qproposedsearchresult/tst_qproposedsearchresult.cpp
+++ tests/auto/qproposedsearchresult/tst_qproposedsearchresult.cpp
@@ -1,5 +1,5 @@
 // Copyright (C) 2016 Aaron McCarthy <mccarthy.aaron@gmail.com>
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
 
 #include <QtCore/QString>
 #include <QtLocation/QPlaceProposedSearchResult>
