ed0126c0fa582a752a133379326c15a17cc5c638
Lucie Gérard <lucie.gerard@qt.io>
2024-03-15 15:25:20 +0100

    Correct license for module and plugin files
    According to QUIP-18 [1], all module and
    plugin files should be
    LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
    or
    LicenseRef-Qt-Commercial OR GPL-3.0-only
    
    Also, LGPL and non LGPL should not be mixed in a given directory
    
    [1]: https://contribute.qt-project.org/quips/18
    
    Pick-to: 6.7
    Task-number: QTBUG-121787
    Change-Id: I10b52519f7eed93d621825a7f68c517b09c96a01
    Reviewed-by: Kai Köhne <kai.koehne@qt.io>
    Reviewed-by: Matthias Rauter <matthias.rauter@qt.io>

--- src/location/declarativemaps/qdeclarativegeojsondata.cpp
+++ src/location/declarativemaps/qdeclarativegeojsondata.cpp
@@ -1,6 +1,6 @@
 // Copyright (C) 2019 Julian Sherollari <jdotsh@gmail.com>
 // Copyright (C) 2023 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 #include "qdeclarativegeojsondata_p.h"
 
