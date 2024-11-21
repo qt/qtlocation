6569c412b91944f1cff411863a479043fc8b3cfc
Jonas Karlsson <jonas.karlsson@qt.io>
2024-11-21 17:00:29 +0100

    test: baseline: Use QBASELINETEST_MAIN macro
    Change-Id: I6dedc611dbc8c19125cce0295b43a5685661da90
    Reviewed-by: Eirik Aavitsland <eirik.aavitsland@qt.io>
    (cherry picked from commit dea21ab56ea3bf868c7b3489eca41426149381c7)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/baseline/mapitems/tst_baseline_mapitems.cpp
+++ tests/baseline/mapitems/tst_baseline_mapitems.cpp
@@ -231,14 +231,6 @@ bool tst_Baseline_MapItems::renderAndGrab(const QString& qmlFile, const QStringL
     return true;
 }
 
-#define main _realmain
-QTEST_MAIN(tst_Baseline_MapItems)
-#undef main
-
-int main(int argc, char *argv[])
-{
-    QBaselineTest::handleCmdLineArgs(&argc, &argv);
-    return _realmain(argc, argv);
-}
+QBASELINETEST_MAIN(tst_Baseline_MapItems)
 
 #include "tst_baseline_mapitems.moc"
