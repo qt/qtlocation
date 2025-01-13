d278bcdf54d2e33dbcae4b4ccac0a3f89a6a7e3e
Ulf Hermann <ulf.hermann@qt.io>
2025-01-13 08:21:20 +0100

    Update Q_REVISION to two-component revisions
    Otherwise the methods are inacessible.
    
    Amends commit 104e1bb73f19eff5ca5c136bdbcb99e2e4bc196b.
    
    Change-Id: I39ec68516f997826dc4b95b89338cd9c6f0fd802
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    Reviewed-by: Tommi M. Tauriainen
    (cherry picked from commit a58deacf8383133455a82d214e51a8e262d8d6de)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit b872a9d98a96b5d21afd342a14fd3a6ec0369ec5)
    (cherry picked from commit cc6641df448a52e8cd9932c9f213a4341f7110b6)

--- src/location/quickmapitems/qdeclarativegeomapitembase_p.h
+++ src/location/quickmapitems/qdeclarativegeomapitembase_p.h
@@ -99,8 +99,8 @@ public:
 
 Q_SIGNALS:
     void mapItemOpacityChanged();
-    Q_REVISION(12) void addTransitionFinished();
-    Q_REVISION(12) void removeTransitionFinished();
+    Q_REVISION(5, 12) void addTransitionFinished();
+    Q_REVISION(5, 12) void removeTransitionFinished();
     void lodThresholdChanged();
 
 protected Q_SLOTS:
